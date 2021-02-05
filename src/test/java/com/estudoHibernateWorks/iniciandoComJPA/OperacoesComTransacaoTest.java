c86f9e68be9572c7b7fc5013280a4ebff8071b66
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-05 19:36:17 -0300

    2.9
    Inserindo o primeiro objeto com o metodo persist

--- src/test/java/com/estudoHibernateWorks/iniciandoComJPA/OperacoesComTransacaoTest.java
+++ src/test/java/com/estudoHibernateWorks/iniciandoComJPA/OperacoesComTransacaoTest.java
@@ -1,11 +1,39 @@
 package com.estudoHibernateWorks.iniciandoComJPA;
 
+
+import java.math.BigDecimal;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.estudoHibernateWorks.EntityManagerTest;
 import com.estudoHibernateWorks.model.Produto;
 
 public class OperacoesComTransacaoTest extends EntityManagerTest{
 
+	@Test
+	public void inserirOPrimeiroObjeto() {
+		Produto produto = new Produto();
+		
+		produto.setId(2);
+		produto.setNome("Camera Canon");
+		produto.setDescricao("A melhor definicao para suas fotos" );
+		produto.setPreco(new BigDecimal(5000));
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(produto);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+		Assert.assertEquals("Camera Canon", produtoVerificacao.getNome());
+		
+		
+	}
 	
+	@Test
 	public void abrirEFecharATransacao() {
 //		Produto produto = new Produto();
 		
