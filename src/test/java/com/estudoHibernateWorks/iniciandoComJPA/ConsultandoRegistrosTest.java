f604132c3bd7c70fcfa855c2a7976141d02db17a
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-05 18:58:38 -0300

    2.7
    Criando uma classe genérica para testes

--- src/test/java/com/estudoHibernateWorks/iniciandoComJPA/ConsultandoRegistrosTest.java
+++ src/test/java/com/estudoHibernateWorks/iniciandoComJPA/ConsultandoRegistrosTest.java
@@ -1,56 +1,31 @@
 package com.estudoHibernateWorks.iniciandoComJPA;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import org.junit.Assert;
+import org.junit.Test;
 
-import org.junit.*;
-
-
+import com.estudoHibernateWorks.EntityManagerTest;
 import com.estudoHibernateWorks.model.Produto;
 
-public class ConsultandoRegistrosTest {
-	  private static EntityManagerFactory entityManagerFactory;
+public class ConsultandoRegistrosTest extends EntityManagerTest {
 
-	    private EntityManager entityManager;
-
-	    @BeforeClass
-	    public static void setUpBeforeClass() {
-	        entityManagerFactory = Persistence
-	                .createEntityManagerFactory("Ecommerce-PU");
-	    }
-
-	    @AfterClass
-	    public static void tearDownAfterClass() {
-	        entityManagerFactory.close();
-	    }
-
-	    @Before
-	    public void setUp() {
-	        entityManager = entityManagerFactory.createEntityManager();
-	    }
-
-	    @After
-	    public void tearDown() {
-	        entityManager.close();
-	    }
-
-	    @Test
-	    public void busarPorIdentificador() {
-	        Produto produto = entityManager.find(Produto.class, 1);
+	@Test
+	public void busarPorIdentificador() {
+		Produto produto = entityManager.find(Produto.class, 1);
 //	        Produto produto = entityManager.getReference(Produto.class, 1);
 
-	        Assert.assertNotNull(produto);
-	        Assert.assertEquals("Kindle", produto.getNome());
-	    }
-
-	    @Test
-	    public void atualizarAReferencia() {
-	        Produto produto = entityManager.find(Produto.class, 1);
-	        produto.setNome("Microfone Samson");
-
-	        entityManager.refresh(produto);
-
-	        Assert.assertEquals("Kindle", produto.getNome());
-	    }
+		System.out.println(">>>>>>>>>>" + produto.getNome());
+		Assert.assertNotNull(produto);
+		Assert.assertEquals("Kindle", produto.getNome());
 	}
+
+	@Test
+	public void atualizarAReferencia() {
+		Produto produto = entityManager.find(Produto.class, 1);
+		produto.setNome("Microfone Samson");
+
+		System.out.println(">>>>>>>>>>" + produto.getNome());
+		entityManager.refresh(produto);
+
+		Assert.assertEquals("Kindle", produto.getNome());
+	}
+}
