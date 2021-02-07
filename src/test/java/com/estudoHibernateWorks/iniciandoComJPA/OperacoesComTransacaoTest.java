9d99591623e806daa19185d46684e86d2ce2b25d
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-06 21:25:18 -0300

    2.11
    Atualizando objetos com o método merge

--- src/test/java/com/estudoHibernateWorks/iniciandoComJPA/OperacoesComTransacaoTest.java
+++ src/test/java/com/estudoHibernateWorks/iniciandoComJPA/OperacoesComTransacaoTest.java
@@ -11,6 +11,27 @@ import com.estudoHibernateWorks.model.Produto;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
 	@Test
+	public void atualizacaorObjeto() {
+		Produto produto = new Produto();
+
+		produto.setId(1);
+		produto.setNome("Kindle Paperwhite");
+		produto.setDescricao("Conheca o novo Kindle");
+		produto.setPreco(new BigDecimal(599));
+
+		entityManager.getTransaction().begin();
+		entityManager.merge(produto);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertNotNull(produtoVerificacao);
+		Assert.assertEquals("Kindle Paperwhite", produtoVerificacao.getNome());
+
+	}
+
+	@Test
 	public void removerObject() {
 		Produto produto = entityManager.find(Produto.class, 3);
 
