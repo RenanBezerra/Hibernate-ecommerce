27d2b13a6d019c1b9da1bfd4bdc52fa23fffc836
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-10 21:36:24 -0300

    3.5
    Mapeando enumeracoes com @Enumerated

---
+++ src/test/java/com/estudohibernateworkstest/mapeamentobasico/MapeandoEnumeracoesTest.java
@@ -0,0 +1,29 @@
+package com.estudohibernateworkstest.mapeamentobasico;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.estudohibernateworks.model.Cliente;
+import com.estudohibernateworks.model.SexoCliente;
+import com.estudohibernateworkstest.EntityManagerTest;
+
+public class MapeandoEnumeracoesTest extends EntityManagerTest{
+	
+	@Test
+	public void testarEnum() {
+		Cliente cliente = new Cliente();
+		cliente.setId(4);
+		cliente.setNome("Jose Mineiro");
+		cliente.setSexo(SexoCliente.MASCULINO);
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+		Assert.assertNotNull(clienteVerificacao);
+	}
+
+}
