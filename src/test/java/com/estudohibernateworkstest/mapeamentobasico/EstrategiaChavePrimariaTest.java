2219afeeb6795043a8884c96d37bbea819a073bf
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-11 21:32:35 -0300

    3.7
    Conhecendo as estrategias para geracao de identificador com @GeneratedValue

---
+++ src/test/java/com/estudohibernateworkstest/mapeamentobasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,26 @@
+package com.estudohibernateworkstest.mapeamentobasico;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.estudohibernateworks.model.Categoria;
+import com.estudohibernateworkstest.EntityManagerTest;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest{
+
+	@Test
+	public void testarEstrategiaAuto() {
+		
+		Categoria categoria = new Categoria();
+		categoria.setNome("Eletronicos");
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(categoria);
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+		Assert.assertNotNull(categoriaVerificacao);
+	}
+}
