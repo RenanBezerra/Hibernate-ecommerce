a8a60cd7438b210acc39df2f8e6e7bbbdbe60170
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-23 18:48:42 -0300

    5.1
    Estados e ciclo de vida dos objetos

---
+++ src/test/java/com/estudohibernateworkstest/conehcendoentitymanager/EstadosECiclosDeVidaTest.java
@@ -0,0 +1,25 @@
+package com.estudohibernateworkstest.conehcendoentitymanager;
+
+import org.junit.Test;
+
+import com.estudohibernateworks.model.Categoria;
+import com.estudohibernateworkstest.EntityManagerTest;
+
+public class EstadosECiclosDeVidaTest extends EntityManagerTest {
+
+	@Test
+	public void analisarEstados() {
+		Categoria categoriaNovo = new Categoria();
+
+		Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo);
+
+		Categoria categoriaGerenciada = entityManager.find(Categoria.class, 1);
+
+		entityManager.remove(categoriaGerenciada);
+		entityManager.persist(categoriaGerenciada);
+
+		entityManager.detach(categoriaGerenciada);
+
+	}
+
+}
