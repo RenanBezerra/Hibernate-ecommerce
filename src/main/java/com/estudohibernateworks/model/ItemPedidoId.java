9be760bbc2bd560529de9919e02419aef8e433f9
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-02-28 20:32:01 -0300

    6.5
    Mapeando chave composta com @Embeddedle e @Embeddable mais aconselhavel essa estrategia

--- src/main/java/com/estudohibernateworks/model/ItemPedidoId.java
+++ src/main/java/com/estudohibernateworks/model/ItemPedidoId.java
@@ -2,6 +2,9 @@ package com.estudohibernateworks.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,6 +14,7 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class ItemPedidoId implements Serializable {
 
 	/**
@@ -19,8 +23,10 @@ public class ItemPedidoId implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@EqualsAndHashCode.Include
+	@Column(name = "pedido_id")
 	private Integer pedidoId;
 
 	@EqualsAndHashCode.Include
+	@Column(name = "produto_id")
 	private Integer produtoId;
 }
