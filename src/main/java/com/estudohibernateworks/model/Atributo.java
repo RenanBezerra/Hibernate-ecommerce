c31be4027c9984db10c549388fb3108d5fd44a92
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-03 22:40:36 -0300

    6.10
    Mapeando colecoes de objetos embutidos com @ElementCollection

---
+++ src/main/java/com/estudohibernateworks/model/Atributo.java
@@ -0,0 +1,19 @@
+package com.estudohibernateworks.model;
+
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class Atributo {
+
+	private String nome;
+	
+	private String valor;
+	
+}
--- src/test/java/com/estudohibernateworkstest/mapeamentoavancado/ElementCollectionTest.java
+++ src/test/java/com/estudohibernateworkstest/mapeamentoavancado/ElementCollectionTest.java
@@ -1,10 +1,12 @@
 package com.estudohibernateworkstest.mapeamentoavancado;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.estudohibernateworks.model.Atributo;
 import com.estudohibernateworks.model.Produto;
 import com.estudohibernateworkstest.EntityManagerTest;
 
@@ -23,5 +25,21 @@ public class ElementCollectionTest extends EntityManagerTest {
 
 		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
 		Assert.assertFalse(produtoVerificacao.getTags().isEmpty());
+	}
+	
+	@Test
+	public void aplicarAtributos() {
+		entityManager.getTransaction().begin();
+		
+		Produto produto = entityManager.find(Produto.class, 1);
+		produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
+		
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertFalse(produtoVerificacao.getAtributos().isEmpty());
+		
 	}
 }
