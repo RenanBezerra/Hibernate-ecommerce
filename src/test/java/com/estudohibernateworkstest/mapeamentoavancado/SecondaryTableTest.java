ce33ef390f56a09b70d6f15216063eac431cf07b
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-05 23:53:12 -0300

    6.114
    Mapeando tabela secundaria com @SecondaryTable

---
+++ src/test/java/com/estudohibernateworkstest/mapeamentoavancado/SecondaryTableTest.java
@@ -0,0 +1,30 @@
+package com.estudohibernateworkstest.mapeamentoavancado;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.estudohibernateworks.model.Cliente;
+import com.estudohibernateworks.model.SexoCliente;
+import com.estudohibernateworkstest.EntityManagerTest;
+
+public class SecondaryTableTest extends EntityManagerTest {
+
+	@Test
+	public void salvarCliente() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Carlos Finotti");
+		cliente.setSexo(SexoCliente.MASCULINO);
+		cliente.setDataNascimento(LocalDate.of(1990, 1, 1));
+
+		entityManager.getTransaction().begin();
+		entityManager.persist(cliente);
+		entityManager.getTransaction().commit();
+
+		entityManager.clear();
+
+		Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+		Assert.assertNotNull(clienteVerificacao.getSexo());
+	}
+}
