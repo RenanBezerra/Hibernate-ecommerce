e218ca63395f59ed9832dab716542c4d98e7ad59
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-24 22:49:20 -0300

    8.7
    Entendendo a remocao em cascata com @ManyToMany

--- src/test/java/com/estudohibernateworkstest/operacoesemcascata/CascadeTypeRemoveTest.java
+++ src/test/java/com/estudohibernateworkstest/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -1,14 +1,32 @@
 package com.estudohibernateworkstest.operacoesemcascata;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import com.estudo.hibernate.works.model.ItemPedido;
 import com.estudo.hibernate.works.model.ItemPedidoId;
 import com.estudo.hibernate.works.model.Pedido;
+import com.estudo.hibernate.works.model.Produto;
 import com.estudohibernateworkstest.EntityManagerTest;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
 
+	@Test
+	public void removerRelacaoProdutoCategoria() {
+		Produto produto = entityManager.find(Produto.class, 1);
+		
+		Assert.assertFalse(produto.getCategorias().isEmpty());
+		
+		entityManager.getTransaction().begin();
+		produto.getCategorias().clear();
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		Assert.assertTrue(produtoVerificacao.getCategorias().isEmpty());
+	}
+	
 	// @Test
 	public void removerPedidoEItens() {
 		Pedido pedido = entityManager.find(Pedido.class, 1);
