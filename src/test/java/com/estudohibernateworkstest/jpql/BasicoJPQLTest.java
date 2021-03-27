814961e789e0e6e123525268c643196c95f84232
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-27 10:42:57 -0300

    9.4
    Trabalhando com projeções

--- src/test/java/com/estudohibernateworkstest/jpql/BasicoJPQLTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/BasicoJPQLTest.java
@@ -15,12 +15,24 @@ import com.estudohibernateworkstest.EntityManagerTest;
 public class BasicoJPQLTest extends EntityManagerTest {
 
 	@Test
+	public void projetarOResultado() {
+		String jpql = "select id, nome from Produto";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertTrue(lista.get(0).length == 2);
+		
+		lista.forEach(arr -> System.out.println(arr[0]+ ", "+ arr[1]));
+	}
+	
+	@Test
 	public void selecionarUmAtributoParaRetorno() {
 		String jpql = "select p.nome from Produto p";
 		
 		TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
 		List<String> lista = typedQuery.getResultList();
-		Assert.assertTrue(String.class.equals(lista.get(0)));
+		Assert.assertTrue(String.class.equals(lista.get(0).getClass()));
 		
 		String jpqlCliente = "select p.cliente from Pedido p";
 		TypedQuery<Cliente> typedQueryCliente = entityManager.createQuery(jpqlCliente, Cliente.class);
