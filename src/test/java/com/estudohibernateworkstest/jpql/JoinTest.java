e62fcabb3c85ba0643937159ef4371d8bb016c12
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-27 12:24:54 -0300

    9.8
    Fazendo o join e usando o fetch

--- src/test/java/com/estudohibernateworkstest/jpql/JoinTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/JoinTest.java
@@ -7,10 +7,26 @@ import javax.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.estudo.hibernate.works.model.Pedido;
 import com.estudohibernateworkstest.EntityManagerTest;
 
 public class JoinTest extends EntityManagerTest {
 
+	@Test
+	public void usarJoinFetch() {
+		String jpql = "select p from Pedido p "
+				+ " left join fetch p.pagamento "
+				+ " join fetch p.cliente "
+				+ " left join fetch p.notaFiscal "
+				;
+		
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+		
+		List<Pedido> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+	
 	@Test
 	public void fazerLeftJoin() {
 		String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
