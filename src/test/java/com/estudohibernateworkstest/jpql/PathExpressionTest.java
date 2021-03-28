d614d5d16d1d5c9d9baa1d6cba6b7262cceba5cf
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-28 11:01:20 -0300

    9.9
    Entendendo as Path Expressions

---
+++ src/test/java/com/estudohibernateworkstest/jpql/PathExpressionTest.java
@@ -0,0 +1,23 @@
+package com.estudohibernateworkstest.jpql;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.estudohibernateworkstest.EntityManagerTest;
+
+public class PathExpressionTest extends EntityManagerTest {
+
+	@Test
+	public void usarPathExpressions() {
+		String jpql = "select p.cliente.nome from Pedido p";
+		
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
+}
