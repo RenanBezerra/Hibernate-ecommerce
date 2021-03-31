9e081ca6b9f253ac5830c5ed4e2a7d5caed22c6e
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-03-31 11:26:43 -0300

    9.19
    Ordenando os resultados da consulta

--- src/test/java/com/estudohibernateworkstest/jpql/BasicoJPQLTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/BasicoJPQLTest.java
@@ -16,6 +16,18 @@ import com.estudohibernateworkstest.EntityManagerTest;
 public class BasicoJPQLTest extends EntityManagerTest {
 
 	@Test
+	public void ordenarResultados() {
+		String jpql = "select c from Cliente c order by c.nome desc";
+
+		TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+
+		List<Cliente> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+
+		lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+	}
+
+	@Test
 	public void projetarNoDTO() {
 		String jpql = "select new com.estudo.hibernate.works.dto.ProdutoDTO(id,nome) from Produto";
 
