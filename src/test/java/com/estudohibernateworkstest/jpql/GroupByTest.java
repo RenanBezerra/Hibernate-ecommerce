9d4d21fed7c6241513d95c454b65a9f943131abc
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-04-02 12:39:25 -0300

    9.29
    Exercicio usando group by

--- src/test/java/com/estudohibernateworkstest/jpql/GroupByTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/GroupByTest.java
@@ -21,9 +21,27 @@ public class GroupByTest extends EntityManagerTest {
 //				+ " from Pedido p group by year(p.dataCriacao), month(p.dataCriacao) ";
 
 		// Total de vendas por categoria
-		String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip "
-				+ " join ip.produto pro join pro.categorias c "
-				+ " group by c.id";
+//		String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip "
+//				+ " join ip.produto pro join pro.categorias c "
+//				+ " group by c.id";
+		
+		// Total de vendas por cliente 
+//		String jpql = "select c.nome , sum(ip.precoProduto) from ItemPedido ip "
+//				+ " join ip.pedido pe join pe.cliente c "
+//				+ " group by c.id";
+	
+		// Total de vendas por dia e por categoria
+//		String jpql = "select pedi.dataCriacao, sum(ip.precoProduto) from Pedido pedi,"
+//				+ " join pedi.itens ip "
+//		+ " join ip.produto pro join pro.categorias c "
+//		+ " group by pedi.dataCriacao and c.id";
+
+		 String jpql = "select " +
+	                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+	                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
+	                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
+	                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+	                " order by p.dataCriacao, c.nome ";
 		
 		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
 		
