52ba9dc98a8926a97572e664053159d2b0711c25
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-04-03 15:20:12 -0300

    9.30
    Usando a clausula where com group by

--- src/test/java/com/estudohibernateworkstest/jpql/GroupByTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/GroupByTest.java
@@ -12,6 +12,34 @@ import com.estudohibernateworkstest.EntityManagerTest;
 public class GroupByTest extends EntityManagerTest {
 
 	@Test
+	public void agruparEFiltrarResultado() {
+		// Total de vendas por mes where ano da venda = ano atual
+//		String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)),sum(p.total)"
+//				+ " from Pedido p "
+//				+ " where year(p.dataCriacao) = year(current_date) "
+//				+ " group by year(p.dataCriacao), month(p.dataCriacao) ";
+		
+		//Total de vendas por categoria where ano da venda = ano atual
+//		String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip "
+//				+ " join ip.produto pro join pro.categorias c join ip.pedido p "
+//				+ " where year(p.dataCriacao) = year(current_date) and month (p.dataCriacao) = month(current_date) "
+//				+ " group by c.id";
+		
+		//Total de vendas por cliente where ano= anoAtual e mes > mes atual -3 dias
+		String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip "
+				+ " join ip.pedido p join p.cliente c join ip.pedido p "
+				+ " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) -3) "
+				+ " group by c.id";
+		TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		
+		List<Object[]> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+		lista.forEach(arr -> System.out.println("groupBy com where: " + arr[0] +","+ arr[1]));
+	}
+	
+	@Test
 	public void agruparResultado() {
 		//Quantidade de produtos
 //		String jpql = "select c.nome, count(p.id) from Categoria c join c.produtos p group by c.id";
@@ -31,10 +59,10 @@ public class GroupByTest extends EntityManagerTest {
 //				+ " group by c.id";
 	
 		// Total de vendas por dia e por categoria
-//		String jpql = "select pedi.dataCriacao, sum(ip.precoProduto) from Pedido pedi,"
-//				+ " join pedi.itens ip "
-//		+ " join ip.produto pro join pro.categorias c "
-//		+ " group by pedi.dataCriacao and c.id";
+//		String jpql = "select day(pedi.dataCriacao), sum(ip.precoProduto) from Pedido pedi,"
+//				+ " join pedi.itens it "
+//		+ " join it.produto pro join pro.categorias c "
+//		+ " group by day(pedi.dataCriacao), c.id";
 
 		 String jpql = "select " +
 	                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
