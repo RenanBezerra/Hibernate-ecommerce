bddbefa26d15e4fee3cb002c5e8bc4f7c90b8cbb
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-04-06 22:47:45 -0300

    9.35
    Criando subqueries 2

--- src/test/java/com/estudohibernateworkstest/jpql/SubqueriesTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/SubqueriesTest.java
@@ -7,6 +7,8 @@ import javax.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.estudo.hibernate.works.model.Cliente;
+import com.estudo.hibernate.works.model.Produto;
 import com.estudohibernateworkstest.EntityManagerTest;
 
 public class SubqueriesTest extends EntityManagerTest{
@@ -17,17 +19,26 @@ public class SubqueriesTest extends EntityManagerTest{
 		//String jpql = "";
 		
 		// Bons clientes. Versao 2.
-		//String jpql = "";
+		String jpql = "select c from Cliente c where "
+				+ " 500 < (select sum(p.total) from Pedido p where p.cliente = c)";
+		
+//		String jpql = "select c from Cliente c where "
+//				+ " 500 < (select sum(p.total) from c.pedidos p)";
+//		
 		
 		// Bons clientes. Versao 2.
-		//String jpql = "";
+//		String jpql = "select p from Pedido p where "
+//				+ " p.total > (select avg(total) from Pedido)";
 		
-		// o produto ou os produtos mais caros da base.
-		
-	String jpql = "";
-	TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+		// o produto ou os produtos mais caros da base.	
+//	String jpql = "select p from Produto p where "
+//			+ "p.preco = (select max(preco) from Produto)";
 	
-	List<Object[]> lista = typedQuery.getResultList();
+	TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+	
+	List<Cliente> lista = typedQuery.getResultList();
 	Assert.assertFalse(lista.isEmpty());
+	
+	lista.forEach(obj -> System.out.println("ID: " + obj.getId() + ", Preco: "+ obj.getNome()));
 	}
 }
