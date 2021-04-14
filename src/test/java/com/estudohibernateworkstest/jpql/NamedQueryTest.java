7f732e17df4f7ddf9c332deec5e9ef235c76a79a
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-04-13 22:30:36 -0300

    9.49
    Externailzando queries em um arquivo XML

--- src/test/java/com/estudohibernateworkstest/jpql/NamedQueryTest.java
+++ src/test/java/com/estudohibernateworkstest/jpql/NamedQueryTest.java
@@ -7,11 +7,43 @@ import javax.persistence.TypedQuery;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.estudo.hibernate.works.model.Pedido;
 import com.estudo.hibernate.works.model.Produto;
 import com.estudohibernateworkstest.EntityManagerTest;
 
 public class NamedQueryTest extends EntityManagerTest {
 
+	@Test
+	public void executarConsultaArquivoXMLEspecificoProduto() {
+		TypedQuery<Produto> typedQuery = entityManager
+				.createNamedQuery("Produto.todos", Produto.class);
+		
+		List<Produto> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+	@Test
+	public void executarConsultaArquivoXMLEspecificoPedido() {
+		TypedQuery<Pedido> typedQuery = entityManager
+				.createNamedQuery("Pedido.todos", Pedido.class);
+		
+		List<Pedido> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+	}
+	
+	@Test
+	public void executarConsultaArquivoXML() {
+		TypedQuery<Pedido> typedQuery = entityManager
+				.createNamedQuery("Pedido.listar", Pedido.class);
+		
+		List<Pedido> lista = typedQuery.getResultList();
+		
+		Assert.assertFalse(lista.isEmpty());
+		
+	}
+	
 	@Test
 	public void executarConsulta() {
 		TypedQuery<Produto> typedQuery = entityManager
