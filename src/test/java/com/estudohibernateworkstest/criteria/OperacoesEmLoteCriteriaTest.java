aa08685d00f40ae4d86f61613489e2731923a2a3
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-05-11 20:10:29 -0300

    10.49
    Removendo objetos em lote

--- src/test/java/com/estudohibernateworkstest/criteria/OperacoesEmLoteCriteriaTest.java
+++ src/test/java/com/estudohibernateworkstest/criteria/OperacoesEmLoteCriteriaTest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaUpdate;
 import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
@@ -17,31 +18,50 @@ import com.estudo.hibernate.works.model.Produto;
 import com.estudo.hibernate.works.model.Produto_;
 import com.estudohibernateworkstest.EntityManagerTest;
 
-public class OperacoesEmLoteCriteriaTest extends EntityManagerTest{
+public class OperacoesEmLoteCriteriaTest extends EntityManagerTest {
+
+	@Test
+	public void removerEmLote() {
+		entityManager.getTransaction().begin();
+
+		// String jpql = "delete from Produto p where p.id between 5 and 12";
+
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaDelete<Produto> criteriaDelete = criteriaBuilder.createCriteriaDelete(Produto.class);
+		Root<Produto> root = criteriaDelete.from(Produto.class);
+
+		criteriaDelete.where(criteriaBuilder.between(root.get(Produto_.id), 5, 12));
+
+		Query query = entityManager.createQuery(criteriaDelete);
+		query.executeUpdate();
+
+		entityManager.getTransaction().commit();
+	}
 
 	@Test
 	public void atualizarEmLote() {
 		entityManager.getTransaction().begin();
-		
+
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
 		CriteriaUpdate<Produto> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(Produto.class);
 		Root<Produto> root = criteriaUpdate.from(Produto.class);
-		
-		criteriaUpdate.set(root.get(Produto_.preco), criteriaBuilder.prod(root.get(Produto_.preco), new BigDecimal("1.1")));
-		
+
+		criteriaUpdate.set(root.get(Produto_.preco),
+				criteriaBuilder.prod(root.get(Produto_.preco), new BigDecimal("1.1")));
+
 		Subquery<Integer> subquery = criteriaUpdate.subquery(Integer.class);
 		Root<Produto> subqueryRoot = subquery.correlate(root);
 		Join<Produto, Categoria> joinCategoria = subqueryRoot.join(Produto_.categorias);
 		subquery.select(criteriaBuilder.literal(1));
 		subquery.where(criteriaBuilder.equal(joinCategoria.get(Categoria_.id), 2));
-		
+
 		criteriaUpdate.where(criteriaBuilder.exists(subquery));
-		
+
 		Query query = entityManager.createQuery(criteriaUpdate);
 		query.executeUpdate();
-		
+
 		entityManager.getTransaction().commit();
-		
+
 	}
-	
+
 }
