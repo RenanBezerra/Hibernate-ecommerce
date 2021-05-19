c8f998a4ed5bcd243c402fa20604df9fcd7f8c1e
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-05-18 22:28:58 -0300

    11.13
    Atualizando registros com procedures

--- src/test/java/com/estudohibernateworkstest/consultasnativas/StoredProceduresTest.java
+++ src/test/java/com/estudohibernateworkstest/consultasnativas/StoredProceduresTest.java
@@ -1,5 +1,6 @@
 package com.estudohibernateworkstest.consultasnativas;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.ParameterMode;
@@ -11,8 +12,32 @@ import org.junit.Test;
 import com.estudo.hibernate.works.model.Cliente;
 import com.estudohibernateworkstest.EntityManagerTest;
 
+import javassist.expr.NewArray;
+
 public class StoredProceduresTest extends EntityManagerTest {
 
+	@Test
+	public void atualizarPrecoProduto() {
+		StoredProcedureQuery storedProcedureQuery = entityManager
+				.createStoredProcedureQuery("ajustar_preco_produto", Cliente.class);
+		
+		storedProcedureQuery.registerStoredProcedureParameter("produto_id", Integer.class, ParameterMode.IN);
+		
+		storedProcedureQuery.registerStoredProcedureParameter("percentual_ajuste", BigDecimal.class, ParameterMode.IN);
+		
+		storedProcedureQuery.registerStoredProcedureParameter("preco_ajustado", BigDecimal.class, ParameterMode.OUT);
+		
+		storedProcedureQuery.setParameter("produto_id", 1);
+		
+		storedProcedureQuery.setParameter("percentual_ajuste", new BigDecimal("0.1"));
+		
+		BigDecimal precoAjustado = (BigDecimal) storedProcedureQuery
+				.getOutputParameterValue("preco_ajustado");
+		
+		Assert.assertEquals(new BigDecimal("878.9"), precoAjustado);
+	
+	}
+	
 	@Test
 	public void receberListaDaProcedure() {
 		StoredProcedureQuery storedProcedureQuery = entityManager.createStoredProcedureQuery("compraram_acima_media",
