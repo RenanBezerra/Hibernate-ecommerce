5c0bea0dd16146d9ea74437b2d77e900c42f6fdc
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-05-26 23:01:54 -0300

    12.12
    Configurando o Entity Graph atraves da anotacao @NamedEntityGraph

--- src/test/java/com/estudohibernateworkstest/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/estudohibernateworkstest/detalhesimportantes/EntityGraphTest.java
@@ -18,27 +18,50 @@ import com.estudohibernateworkstest.EntityManagerTest;
 public class EntityGraphTest extends EntityManagerTest {
 
 	@Test
-	public void buscarAtributosEssenciaisDePedidos02() {
-		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
-		entityGraph.addAttributeNodes(Pedido_.dataCriacao, Pedido_.status, Pedido_.total);
-		
-		Subgraph<Cliente> subgraphCliente = entityGraph
-				.addSubgraph(Pedido_.cliente);
-		subgraphCliente.addAttributeNodes(Cliente_.nome, Cliente_.cpf);
-		
-		TypedQuery<Pedido> typedQuery = entityManager
-				.createQuery("select p from Pedido p", Pedido.class);
+	public void buscarAtributosEssenciaisComNameEntityGraph() {
+		EntityGraph<?> entityGraph = entityManager.createEntityGraph("Pedido.dadosEssenciais");
+		entityGraph.addAttributeNodes("pagamento");
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
 		typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
 		List<Pedido> lista = typedQuery.getResultList();
 		Assert.assertFalse(lista.isEmpty());
 	}
-	
+
+	@Test
+	public void buscarAtributosEssenciaisDePedidos03() {
+		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+		entityGraph.addAttributeNodes(Pedido_.dataCriacao, Pedido_.status, Pedido_.total);
+
+		Subgraph<Cliente> subgraphCliente = entityGraph.addSubgraph(Pedido_.cliente);
+		subgraphCliente.addAttributeNodes(Cliente_.nome, Cliente_.cpf);
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+		List<Pedido> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
+
+	@Test
+	public void buscarAtributosEssenciaisDePedidos02() {
+		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+		entityGraph.addAttributeNodes("dataCriacao", "status", "total");
+
+		Subgraph<Cliente> subgraphCliente = entityGraph.addSubgraph("cliente", Cliente.class);
+		subgraphCliente.addAttributeNodes("nome", "cpf");
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+		typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+		List<Pedido> lista = typedQuery.getResultList();
+		Assert.assertFalse(lista.isEmpty());
+	}
+
 	@Test
 	public void buscarAtributosEssenciaisDePedido() {
-		
+
 		EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
-		entityGraph.addAttributeNodes("dataCriacao", "status", "total","notaFiscal", "cliente");
-		
+		entityGraph.addAttributeNodes("dataCriacao", "status", "total", "notaFiscal", "cliente");
+
 		/*
 		 * Map<String, Object> properties = new HashMap<>();
 		 * //properties.put("javax.persistence.fetchgraph", entityGraph);
@@ -47,12 +70,11 @@ public class EntityGraphTest extends EntityManagerTest {
 		 * Pedido pedido = entityManager.find(Pedido.class, 1, properties);
 		 * Assert.assertNotNull(pedido);
 		 */
-		
-		TypedQuery<Pedido> typedQuery = entityManager
-				.createQuery("select p from Pedido p", Pedido.class);
+
+		TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
 		typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
 		List<Pedido> lista = typedQuery.getResultList();
-		
+
 		Assert.assertFalse(lista.isEmpty());
 	}
 }
