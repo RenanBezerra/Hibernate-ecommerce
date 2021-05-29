ddb5e86bff7f5bba61f786f274b5ecce8bfdcfc7
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-05-29 19:50:19 -0300

    13.6
    Fazendo controle dinamico do cache

--- src/test/java/com/estudohibernateworkstest/cache/CacheTest.java
+++ src/test/java/com/estudohibernateworkstest/cache/CacheTest.java
@@ -1,10 +1,13 @@
 package com.estudohibernateworkstest.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.Cache;
+import javax.persistence.CacheStoreMode;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.sound.midi.Soundbank;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -28,66 +31,100 @@ public class CacheTest {
 		entityManagerFactory.close();
 	}
 
-	
+	@Test
+	public void controleCacheDinamiamente() {
+//		javax.persistence.cache.retrieveMode CacheRetrieveMode
+//		javax.persistence.cache.storeMode CacheStoreMode
+
+		Cache cache = entityManagerFactory.getCache();
+
+		System.out.println("Buscando todos os pedidos.............................");
+		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+		entityManager1.setProperty("javax.persistence.cache.storeMode", CacheStoreMode.BYPASS);
+		entityManager1.createQuery("select p from Pedido p", Pedido.class)
+				.setHint("javax.persistence.cache.storeMode", CacheStoreMode.USE).getResultList();
+
+		System.out.println("Buscando o pedido da ID igual a 2 ........................");
+		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+		Map<String, Object> propriedades = new HashMap<>();
+		// propriedades.put("javax.persistence.cache.storeMode", CacheStoreMode.BYPASS);
+		// propriedades.put("javax.persistence.cache.retrieveMode",
+		// CacheStoreMode.BYPASS);
+
+		entityManager2.find(Pedido.class, 2, propriedades);
+
+		System.out.println("Buscando todos os pedidos (de novo)..........................");
+		EntityManager entityManager3 = entityManagerFactory.createEntityManager();
+		entityManager3.createQuery("select p from Pedido p", Pedido.class)
+				// .setHint("javax.persistence.cache.retrieveMode", CacheRetrieveMode.BYPASS)
+				.getResultList();
+	}
+
+	@Test
+	public void analisarOpcoesCache() {
+		Cache cache = entityManagerFactory.getCache();
+
+		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+
+		System.out.println("Buscando a partir da instancia 1: ");
+		entityManager1.createQuery("select p from Pedido p", Pedido.class).getResultList();
+
+		Assert.assertTrue(cache.contains(Pedido.class, 1));
+	}
+
 	@Test
 	public void verificarSeEstaNoCache() {
 		Cache cache = entityManagerFactory.getCache();
-		
+
 		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
-		
+
 		System.out.println("Buscando a partir da instancia 1: ");
-		entityManager1
-			.createQuery("select p from Pedido p", Pedido.class)
-			.getResultList();
-		
+		entityManager1.createQuery("select p from Pedido p", Pedido.class).getResultList();
+
 		Assert.assertTrue(cache.contains(Pedido.class, 1));
 		Assert.assertTrue(cache.contains(Pedido.class, 2));
 	}
-	
+
 	@Test
 	public void removerDoCache() {
 		Cache cache = entityManagerFactory.getCache();
-		
+
 		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
 		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-		
+
 		System.out.println("Buscando a partir da instancia 1: ");
-		entityManager1
-			.createQuery("select p from Pedido p", Pedido.class)
-			.getResultList();
-		
+		entityManager1.createQuery("select p from Pedido p", Pedido.class).getResultList();
+
 		System.out.println("Removendo do cache");
 		cache.evictAll();
 		cache.evict(Pedido.class);
-		
+
 		System.out.println("Buscando a partir da instancia 2: ");
 		entityManager2.find(Pedido.class, 1);
 		entityManager2.find(Pedido.class, 2);
-		
+
 	}
-	
+
 	@Test
 	public void adicionarPedidosNoCache() {
 		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
 		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-	
+
 		System.out.println("Buscando a partir da instancia 1: ");
-		entityManager1
-			.createQuery("select p from Pedido p ", Pedido.class)
-			.getResultList();
-		
+		entityManager1.createQuery("select p from Pedido p ", Pedido.class).getResultList();
+
 		System.out.println("Buscando a partir da instancia 2:");
 		entityManager2.find(Pedido.class, 1);
 	}
-	
+
 	@Test
 	public void buscarDoCache() {
 		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
 		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
-		
+
 		System.out.println("Buscando a partir da instancia 1:");
 		entityManager1.find(Pedido.class, 1);
-		
+
 		System.out.println("Buscando a partir da instancia 1");
 		entityManager2.find(Pedido.class, 1);
 	}
