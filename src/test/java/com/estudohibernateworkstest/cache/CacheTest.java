d818bfe76e9d63c7e2ea55547897dcb7a75eae56
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-05-28 22:44:47 -0300

    13.4
    Verificando se uma entidade está no cache

--- src/test/java/com/estudohibernateworkstest/cache/CacheTest.java
+++ src/test/java/com/estudohibernateworkstest/cache/CacheTest.java
@@ -7,6 +7,7 @@ import javax.persistence.Persistence;
 import javax.sound.midi.Soundbank;
 
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -27,6 +28,21 @@ public class CacheTest {
 		entityManagerFactory.close();
 	}
 
+	
+	@Test
+	public void verificarSeEstaNoCache() {
+		Cache cache = entityManagerFactory.getCache();
+		
+		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+		
+		System.out.println("Buscando a partir da instancia 1: ");
+		entityManager1
+			.createQuery("select p from Pedido p", Pedido.class)
+			.getResultList();
+		
+		Assert.assertTrue(cache.contains(Pedido.class, 1));
+		Assert.assertTrue(cache.contains(Pedido.class, 2));
+	}
 	
 	@Test
 	public void removerDoCache() {
