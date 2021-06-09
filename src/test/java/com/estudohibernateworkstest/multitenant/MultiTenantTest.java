63d1cf5ea9a14e794c3facc64685c1994ec7d74c
Renan Bezerra Gomes <renanbg90@gmail.com>
2021-06-08 21:26:56 -0300

    15.4.2
    Implementando multinenancy com abordagem por schema 2

--- src/test/java/com/estudohibernateworkstest/multitenant/MultiTenantTest.java
+++ src/test/java/com/estudohibernateworkstest/multitenant/MultiTenantTest.java
@@ -2,6 +2,7 @@ package com.estudohibernateworkstest.multitenant;
 
 import javax.persistence.EntityManager;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.estudo.hibernate.works.model.Produto;
@@ -15,5 +16,15 @@ public class MultiTenantTest extends EntityManagerFactoryTest {
 		EcmCurrentTenantIdentifierResolver.setTenantIdentifier("hibernateworks_ecommerce");
 		EntityManager entityManager1 = entityManagerFactory.createEntityManager();
 		Produto produto1 = entityManager1.find(Produto.class, 1);
+		Assert.assertEquals("Kindle", produto1.getNome());
+		entityManager1.close();
+		
+		EcmCurrentTenantIdentifierResolver.setTenantIdentifier("loja_ecommerce");
+		EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+		Produto produto2 = entityManager2.find(Produto.class, 1);
+		Assert.assertEquals("Kindle Paperwhite", produto2.getNome());
+		entityManager2.close();
+		
+		
 	}
 }
